118e338a6a075764538405a0e87cf05972340760
Pall Palsson <palli@kvikna.com>
2021-02-19 10:39:48 +0000

    use Q_DECLARE_METATYPE "correctly"

--- MetaRegister.cpp
+++ MetaRegister.cpp
@@ -5,12 +5,16 @@
 
 #include "Sender.h"
 
-namespace MyNamespace {
-    Q_DECLARE_METATYPE(MyEnum)
-    Q_DECLARE_METATYPE(MyStruct)
-}
+// Works, but incorrect use of macros
+// namespace MyNamespace {
+//     Q_DECLARE_METATYPE(MyEnum)
+//     Q_DECLARE_METATYPE(MyEnum2)
+// }
 
+// Correct use of macro, but doesn't work
+Q_DECLARE_METATYPE(MyNamespace::MyEnum)
 Q_DECLARE_METATYPE(MyNamespace::MyEnum2)
+Q_DECLARE_METATYPE(MyNamespace::MyStruct)
 Q_DECLARE_METATYPE(MyNamespace::MyStruct2)
 
 void MetaRegister::RegisterAllOfTheThings()
